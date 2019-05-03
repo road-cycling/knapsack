335b4879dcd1430f0ae0a313c215d75755bb7e2a
nieroda <eathotlead@gmail.com>
2019-05-03 00:06:57 -0700

    p2 && 1

--- main.ml
+++ main.ml
@@ -9,8 +9,8 @@ let fileOpen input =
   List.fold2_exn 
     (In_channel.read_lines (input ^ "_w.txt")) 
     (In_channel.read_lines (input ^ "_v.txt"))
-    ~init:(Array.create ~len:0 (0, 0)) ~f:(fun init weight text -> 
-      Array.append init (Array.create ~len:1 ( trimLine weight,  trimLine text ))) 
+    ~init:(Array.create ~len:0 (0, 0)) ~f:(fun init weight value -> 
+      Array.append init (Array.create ~len:1 ( trimLine weight,  trimLine value ))) 
 
 let fileWeight input =
   In_channel.read_all (input ^ "_c.txt") |> trimLine
@@ -21,25 +21,27 @@ let getValue  bag i = let ( _, v ) = bag.(i) in v
 let functionRunner f bag weight =
   let t = Unix.gettimeofday () in
   f bag weight;
-  Printf.printf "Execution time: %fs\n" (Unix.gettimeofday () -. t);
+  Printf.printf "Time Taken: %fs\n" (Unix.gettimeofday () -. t);
 ;;
+
 
 let btbag ~table ~bag = 
   let x = Array.length table - 1 in 
   let y = Array.length table.(0) - 1 in 
   let rec backtrack x y = 
     if table.(x).(y) = 0 then []
-    else if y - getWeight bag (x - 1) < 0 then [getValue bag (x - 1)]
+    else if y - getWeight bag (x - 1) < 0 then [1]
     else match ( table.(x - 1).(y) > table.(x - 1).(y - getWeight bag (x - 1)) + getValue bag (x - 1) ) with
     | true -> backtrack (x - 1) y
-    | false -> (getValue bag (x - 1))::backtrack (x - 1) (y - getWeight bag (x - 1))
+    | false -> x::backtrack (x - 1) (y - getWeight bag (x - 1))
   in backtrack x y ;;
 
 
 let rec recurse lst = 
   match lst with 
-  | [] -> print_endline "Done"
-  | hd::tl -> print_endline (string_of_int hd); recurse tl
+  | [] -> Printf.printf "\n"
+  | hd::tl -> Printf.printf "%d " hd; recurse tl
+  (* | hd::tl -> print_endline (string_of_int hd); recurse tl *)
 ;;
 
 let t1a bag weight = 
@@ -54,10 +56,10 @@ let t1a bag weight =
           ( matrix.(i - 1).(j) )
     done;
   done;
+  Printf.printf "Optimal subset: ";
   recurse (btbag ~table:matrix ~bag:bag);
   print_endline ("Optimal Value " ^ string_of_int (Array.last (Array.last matrix)));
   (* Array.last (Array.last matrix) *)
-
 ;;
 
 let t1b bag weight = 
@@ -75,15 +77,27 @@ let t1b bag weight =
   in knapsack (Array.length bag) weight;
 ;;
 
-let bt ~table ~weight ~value = 
-  let x = Array.length table - 1 in 
-  let y = Array.length table.(0) - 1 in 
-  let rec backtrack x y = 
-    if table.(x).(y) = 0 then []
-    else match ( table.(x - 1).(y) > table.(x - 1).(y - weight.(x - 1)) + value.(x - 1) ) with
-    | true -> backtrack (x - 1) y
-    | false -> (value.(x - 1))::backtrack (x - 1) (y - weight.(x - 1))
-  in backtrack x y ;;
+let foo bag weight =
+  let a = Array.mapi bag 
+    ~f:(fun idx (w, v) -> (
+      float_of_int v /. float_of_int w,
+      getValue bag idx,
+      getWeight bag idx,
+      idx
+    )) in 
+  Array.sort a ~compare:(fun (x, _, _, _) (y, _, _, _) -> if x = y then 0 else if x > y then -1 else 1);
+  Array.iteri ~f:(fun _ (a, b, c, i) -> 
+    print_endline (string_of_float a ^ " " ^ string_of_int b ^ " " ^ string_of_int c ^ " " ^ string_of_int i)
+  ) a;
+  let rec greedy idx currentValue weightLeft = 
+    let ( _ , value, weight, _ ) = a.(idx) in 
+    match (idx = Array.length bag, weightLeft >= 0, weight >= weightLeft) with 
+    | ( _, _, true ) -> currentValue 
+    | ( _, false, _ ) -> print_endline "2"; 0
+    | ( false, true, _ ) -> print_endline "3"; greedy (idx + 1) (currentValue + value) (weightLeft - weight)
+    | ( true, true, _ ) -> print_endline "4"; currentValue
+  in greedy 0 0 weight;
+;;
 
 
 let main = 
@@ -91,8 +105,36 @@ let main =
   input := In_channel.(input_line_exn stdin);
   let weight = fileWeight !input in 
   let backpack = fileOpen !input in 
-  functionRunner t1a backpack weight;
+  print_endline (string_of_int (foo backpack weight))
+  (* functionRunner t1a backpack weight; *)
+  (* foo backpack weight;; *)
   ;;
 
 
 
+(* Traditional Dynamic Programming Optimal value: 1458
+Traditional Dynamic Programming Optimal subset: {1, 3, 5, 7, 8, 9, 14, 15}
+Traditional Dynamic Programming Time Taken: <corresponding output here> *)
+
+
+(* module Htbl = struct 
+  let size = 10
+  let getTbl = Array.init size ~f:(fun _ -> Avltree.empty )
+end  *)
+
+(* let main =  *)
+  (* let _ = Avltree.empty (1, 1) in
+  print_endline "done";
+  print_endline (string_of_int Htbl.size); *)
+  (* let f = Avltree.empty in 
+  let _ = Avltree.add f ~replace:false ~compare:(fun _ _ -> 1) ~key:5 ~data:10 in 
+  let _ = Array.init 10 ~f:(fun _ -> Avltree.empty) in
+  print_endline "done";; *)
+
+(* W      |   v
+1           1
+4           3   
+5           4
+7           5
+
+Vi / Wi *)
